d997f2b54c4d87048617940dd835e9ce5ff785b1
nyt92 <chamroeunlinda12343@gmail.com>
2023-06-21 13:33:09 +0700

    config cors stuff and add sentry for tracking

--- src/app.js
+++ src/app.js
@@ -1,33 +1,63 @@
-import express, { json } from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import 'dotenv/config.js';
+import express, { json } from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+import * as Sentry from "@sentry/node"
 
-import { notFound, errorHandler } from './middlewares.js';
-import api from './routes/index.js';
+dotenv.config()
+
+import { notFound, errorHandler } from "./middlewares.js";
+import api from "./routes/index.js";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.BLOCK_WITH_CORS === 'true'
+        ? !process.env.ALLOWLIST ||
+          process.env.ALLOWLIST === "" ||
+          process.env.ALLOWLIST === "*"
+          ? "*"
+          : process.env.ALLOWLIST.split(",") 
+        : "*",
+  })
+);
 app.use(json());
 
-app.get('/', (req, res) => {
+
+if (process.env.SENTRY_DSN_URL) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN_URL,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Sentry.Integrations.Express({ app }),
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+app.get("/", (req, res) => {
   res.json({
-    status: 200,
-    message: 'IMPROVED amvstrm api',
-    routes: {
-      '/api/v1': 'gogoanime based',
-      '/api/v2': 'gogoanime + anilist based',
-    },
-    github: 'https://github.com/amvstrm/api'
+    code: 200,
+    message: "success",
   });
 });
 
-app.use('/api', api);
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
+app.use("/api", api);
+
+app.use(Sentry.Handlers.errorHandler());
 app.use(notFound);
 app.use(errorHandler);
 

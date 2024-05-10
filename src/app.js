e8ef28ea4d04443756fb24c3e37e2fbedbd42eaf
nyt92 <chamroeunlinda12343@gmail.com>
2024-05-10 21:57:20 +0700

    added env to protect the app & type safety

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-nested-ternary */
 import express, { json, urlencoded } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
-import dotenv from "dotenv";
-import * as Sentry from "@sentry/node";
 
-dotenv.config();
+import * as Sentry from "@sentry/node";
 
 import { notFound, errorHandler } from "./middlewares.js";
 import api from "./routes/index.js";
+import { env } from "./utils/env.js";
+
 
 const app = express();
 
@@ -17,12 +18,12 @@ app.use(helmet());
 app.use(
   cors({
     origin:
-      process.env.BLOCK_WITH_CORS === "true"
-        ? !process.env.ALLOWLIST ||
-          process.env.ALLOWLIST === "" ||
-          process.env.ALLOWLIST === "*"
+      env.data.BLOCK_WITH_CORS === "true"
+        ? !env.data.ALLOWLIST ||
+          env.data.ALLOWLIST === "" ||
+          env.data.ALLOWLIST === "*"
           ? "*"
-          : process.env.ALLOWLIST?.split(",")
+          : env.data.ALLOWLIST.split(",")
         : "*",
   })
 );
@@ -32,16 +33,15 @@ app.use(urlencoded({ extended: true }));
 app.set("trust proxy", 1);
 
 Sentry.init({
-  environment: process.env.NODE_ENV,
-  dsn: process.env.SENTRY_DSN_URL,
+  environment: env.data.NODE_ENV,
+  dsn: env.data.SENTRY_DSN_URL,
   integrations: [
     new Sentry.Integrations.Http({ tracing: true }),
     new Sentry.Integrations.Express({ app }),
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
   tracesSampleRate: 0,
-  sampleRate: 0.45,
-  debug: process.env.NODE_ENV === "production" || !process.env.NODE_ENV || "" ? true : false,
+  sampleRate: 0.10,
 });
 
 

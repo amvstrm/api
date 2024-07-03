0e591e3a82a1cdb95952edc699ca841dedf1c3fa
nyt92 <chamroeunlinda12343@gmail.com>
2024-07-03 21:46:13 +0700

    added info about block all cors

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const port = env.port || 8080;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
+  if (env.data.BLOCK_WITH_CORS === "true") {
+    console.log("Warning: This will block every request even from the localhost. Please manually add domain to the environment variable 'ALLOWLIST'.");
+  }
   /* eslint-enable no-console */
 });
